b6fb81266679345de1417af696ab567280c20559
Alana Kim <alanakim12@gmail.com>
2018-01-31 22:39:55 -0500

    labProgress05.1 - code cleaning
    - refactoring of data cleaning and filtering process has been completed
    in labProgress05.
    - extra files and code lines that kept the original for-loop functions
    can be removed, keeping only the final codes.

--- lab/lab2/part3-underscore-refactor.js
+++ lab/lab2/part3-underscore-refactor.js
@@ -33,14 +33,7 @@
   var minEnrollment = 300;
 
 
-  // clean data
-
-  // for two method comparison create two different set of schools dataset
-  // probably don't want to just assign another variable bc changes in one will be reflected in the other one as well
-
-  /*****************************************
-  refactored: use schools_ dataset
-  *****************************************/
+  // CLEAN DATA //
 
   // 1. Clean string type (i.e. xxxxx - xxxx) zip codes
 
@@ -53,7 +46,6 @@
 
   // function check
   //console.log(_.each(schools, cleanZipcodes));
-
 
   // 2. Clarify school's grade levels by creating four new fields
   // HAS_KINDERGARTEN, HAS_ELEMENTARY, HAS_MIDDLE_SCHOOL, HAS_HIGH_SCHOOL
@@ -76,7 +68,6 @@
 
 
   // 2.2 doesInclude(strInput, substring):
-  //
   var doesInclude = function(strInput, substring) {
     if (strInput.toUpperCase().indexOf(substring) >= 0) {
       return true;
@@ -85,36 +76,23 @@
     }
   };
 
-  // 2.3 cleanGradeLevel():
+  // 2.3 cleanGradeLevel(schoolObj):
   var cleanGradeLevel = function(schoolObj) {
-    if (_.isNumber(schoolObj.GRADE_LEVEL)) { 
+    if (_.isNumber(schoolObj.GRADE_LEVEL)) {
       schoolObj.HAS_KINDERGARTEN = withinGradeRange(schoolObj.GRADE_LEVEL, 0, 1); schoolObj.HAS_ELEMENTARY =  withinGradeRange(schoolObj.GRADE_LEVEL, 1, 6); schoolObj.HAS_MIDDLE_SCHOOL = withinGradeRange(schoolObj.GRADE_LEVEL, 6, 9); schoolObj.HAS_HIGH_SCHOOL = withinGradeRange(schoolObj.GRADE_LEVEL, 9, 13);
     } else {
       schoolObj.HAS_KINDERGARTEN = doesInclude(schoolObj.GRADE_LEVEL, 'K'); schoolObj.HAS_ELEMENTARY = doesInclude(schoolObj.GRADE_LEVEL, 'ELE'); schoolObj.HAS_MIDDLE_SCHOOL = doesInclude(schoolObj.GRADE_LEVEL, 'MID'); schoolObj.HAS_HIGH_SCHOOL = doesInclude(schoolObj.GRADE_LEVEL, 'HIGH');
     }
   };
 
-  // run function 1 and 2 to clean data:
-  _.each(schools_, cleanZipcodes);
-  _.each(schools_, cleanGradeLevel);
+  // run functions 1 and 2 to clean data:
+  _.each(schools, cleanZipcodes);
+  _.each(schools, cleanGradeLevel);
 
 
-  // filter data
-  // has not been refactored; using the given loop-method.
-  //var filtered_data_ = [];
-  //var filtered_out_ = [];
-
+  // FILTER DATA //
 
   // filtering conditions
-
-  /* would something like this useful?
-  var isitOrNot = function(schoolObj, filterKey, condition) {
-    if (condition) {
-      schoolObj.filterKey = true;
-    } else schoolObj.filterKey = false;
-  };
-  */
-
   var isitOpen = function (schoolObj) {
     schoolObj.isOpen = schoolObj.ACTIVE.toUpperCase() === 'OPEN';
   };
@@ -145,16 +123,16 @@
     _.each(data, filterFunction);
   };
 
-  assignFilterCond(schools_, isitOpen);
-  assignFilterCond(schools_, isitPublic);
-  assignFilterCond(schools_, isitSchool);
-  assignFilterCond(schools_, meetsMinimumEnrollment);
+  assignFilterCond(schools, isitOpen);
+  assignFilterCond(schools, isitPublic);
+  assignFilterCond(schools, isitSchool);
+  assignFilterCond(schools, meetsMinimumEnrollment);
   //TODO: how do I chain some of these together?
 
   //filter check
-  console.log(schools_[4]); // yes
+  //console.log(schools_[4]); // yes
 
-  var masterFilter = function (schoolObj) {
+  var applyAllFilters = function (schoolObj) {
     return schoolObj.isOpen &&
       schoolObj.isSchool &&
       schoolObj.meetsMinimumEnrollment &&
@@ -162,134 +140,13 @@
   };  // what would !meetsZipCondition(schoolObj) do?
 
 
-  var filtered_data_ = _.filter(schools_, masterFilter);
-  var filtered_out_ = _.difference(schools_, filtered_data_);
+  var filtered_data = _.filter(schools, applyAllFilters);
+  var filtered_out = _.difference(schools, filtered_data);
 
+  console.log('Included', filtered_data.length);  //273
+  console.log('Excluded:', filtered_out.length);  //277
 
-/*
-  for (var i = 0; i < schools_.length - 1; i++) {
-    isOpen = schools_[i].ACTIVE.toUpperCase() == 'OPEN';
-    isPublic = (schools_[i].TYPE.toUpperCase() !== 'CHARTER' ||
-                schools_[i].TYPE.toUpperCase() !== 'PRIVATE');
-    isSchool = (schools_[i].HAS_KINDERGARTEN ||
-                schools_[i].HAS_ELEMENTARY ||
-                schools_[i].HAS_MIDDLE_SCHOOL ||
-                schools_[i].HAS_HIGH_SCHOOL);
-    meetsMinimumEnrollment = schools_[i].ENROLLMENT > minEnrollment;
-    meetsZipCondition = acceptedZipcodes.indexOf(schools_[i].ZIPCODE) >= 0;
-    filter_condition = (isOpen &&
-                        isSchool &&
-                        meetsMinimumEnrollment &&
-                        !meetsZipCondition);
-
-    if (filter_condition) {
-      filtered_data_.push(schools_[i]);
-    } else {
-      filtered_out_.push(schools_[i]);
-    }
-  }
-  */
-  console.log('Included by underscore:', filtered_data_.length);
-  console.log('Excluded by underscore:', filtered_out_.length);
-
-
-  /*****************************************
-  the given code: data cleaning with a loops
-  *****************************************/
-
-  for (var i = 0; i < schools.length - 1; i++) {
-    // If we have '19104 - 1234', splitting and taking the first (0th) element
-    // as an integer should yield a zip in the format above
-    if (typeof schools[i].ZIPCODE === 'string') {
-      split = schools[i].ZIPCODE.split(' ');
-      normalized_zip = parseInt(split[0]);
-      schools[i].ZIPCODE = normalized_zip;
-    }
-
-    // Check out the use of typeof here — this was not a contrived example.
-    // Someone actually messed up the data entry
-    // AK: so here, we are creating HAS_SCHOOLLEVEL keys using GRADE_LEVEL
-    if (typeof schools[i].GRADE_ORG === 'number') {  // if number (5 objs)
-      schools[i].HAS_KINDERGARTEN = schools[i].GRADE_LEVEL < 1;
-      schools[i].HAS_ELEMENTARY = 1 < schools[i].GRADE_LEVEL < 6;
-      schools[i].HAS_MIDDLE_SCHOOL = 5 < schools[i].GRADE_LEVEL < 9;
-      schools[i].HAS_HIGH_SCHOOL = 8 < schools[i].GRADE_LEVEL < 13;
-    } else {  // otherwise (in case of string)
-      schools[i].HAS_KINDERGARTEN = schools[i].GRADE_LEVEL.toUpperCase().indexOf('K') >= 0;
-      schools[i].HAS_ELEMENTARY = schools[i].GRADE_LEVEL.toUpperCase().indexOf('ELEM') >= 0;
-      schools[i].HAS_MIDDLE_SCHOOL = schools[i].GRADE_LEVEL.toUpperCase().indexOf('MID') >= 0;
-      schools[i].HAS_HIGH_SCHOOL = schools[i].GRADE_LEVEL.toUpperCase().indexOf('HIGH') >= 0;
-    }
-  }
-
-  // filter data
-  var filtered_data = [];
-  var filtered_out = [];
-  for (var i = 0; i < schools.length; i++) {
-    isOpen = schools[i].ACTIVE.toUpperCase() == 'OPEN';
-    isPublic = (schools[i].TYPE.toUpperCase() !== 'CHARTER' ||
-                schools[i].TYPE.toUpperCase() !== 'PRIVATE');
-    isSchool = (schools[i].HAS_KINDERGARTEN ||
-                schools[i].HAS_ELEMENTARY ||
-                schools[i].HAS_MIDDLE_SCHOOL ||
-                schools[i].HAS_HIGH_SCHOOL);
-    meetsMinimumEnrollment = schools[i].ENROLLMENT > minEnrollment;
-    meetsZipCondition = acceptedZipcodes.indexOf(schools[i].ZIPCODE) >= 0;
-    filter_condition = (isOpen &&
-                        isSchool &&
-                        meetsMinimumEnrollment &&
-                        !meetsZipCondition);
-
-    if (filter_condition) {
-      filtered_data.push(schools[i]);
-    } else {
-      filtered_out.push(schools[i]);
-    }
-  }
-  console.log('Included:', filtered_data.length);
-  console.log('Excluded:', filtered_out.length);
-
-
-  /*****************************************
-  running comparison: loop vs. underscore
-  *****************************************/
-// ref: https://stackoverflow.com/questions/19590865/from-an-array-of-objects-extract-value-of-a-property-as-array
-
-  //console.log(filtered_data_);
-  console.log(schools.length, schools_.length);
-
-  var inOriginal = filtered_data.map(a => a.OBJECTID);
-  var outOfOriginal = filtered_out.map(a => a.OBJECTID);
-  var inUnderScore = filtered_data_.map(a => a.OBJECTID);
-  var outOfUnderScore = filtered_out_.map(a => a.OBJECTID);
-
-  console.log('included in both methods', _.intersection(inOriginal, inUnderScore), '\n',
-              'exclued in both methods', _.intersection(outOfOriginal, outOfUnderScore));
-
-  console.log('Only in original filteredData:', _.difference(inOriginal, inUnderScore), '\n',
-              'Only in underscore filteredData:', _.difference(inUnderScore, inOriginal), '\n',
-              'Only excluded from original filteredData:', _.difference(outOfOriginal, outOfUnderScore), '\n',
-              'Only excluded from underscore filteredData:', _.difference(outOfUnderScore, outOfOriginal), '\n');
-
-  console.log('two methods results are the same: ', _.isEqual(inOriginal, inUnderScore));
-
-
-/* zipcode debugging check
-
-  //OBJECTID of those that had filtering issue due to zip code string v. number type:
-  //  [3, 18, 24, 74, 76, 83, 95, 132, 144, 160, 229, 276]
-
-  console.log(acceptedZipcodes);
-  console.log(schools_[0].ZIPCODE);
-  //var theZip = Number(schools_[2].ZIPCODE);
-  var theZip = schools_[0].ZIPCODE;
-  console.log(acceptedZipcodes.indexOf(theZip));
-  console.log(_.findWhere(schools, {OBJECTID: 5}),
-              _.findWhere(schools_, {OBJECTID: 5}));
-*/
-
-
-  // refactored main loop
+  // MAIN LOOP // 
 /*
   //styling options
   var colorStyle = {HAS_HIGH_SCHOOL: '#0000FF', HAS_MIDDLE_SCHOOL: '#00FF00', theRest: '##FF0000'};
